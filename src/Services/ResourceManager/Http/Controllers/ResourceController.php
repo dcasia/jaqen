4e5375966e2524c6957f2aee91196f6f1b6a0e89
milewski <rafael.milewski@gmail.com>
2021-05-30 19:21:28 +0800

    clean up

--- src/Services/ResourceManager/Http/Controllers/ResourceController.php
+++ src/Services/ResourceManager/Http/Controllers/ResourceController.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 
 namespace DigitalCreative\Jaqen\Services\ResourceManager\Http\Controllers;
 
-use DigitalCreative\Jaqen\Http\Requests\BaseRequest;
 use DigitalCreative\Jaqen\Services\ResourceManager\AbstractResource;
 use Illuminate\Support\Collection;
 
@@ -14,10 +13,10 @@ class ResourceController extends Controller
     /**
      * Return a list of all registered resources
      */
-    public function resources(BaseRequest $request): Collection
+    public function resources(): Collection
     {
         return $this->resourceManager
-            ->allAuthorizedResources($request)
+            ->allAuthorizedResources()
             ->map(fn(AbstractResource $resource) => $resource->getDescriptor());
     }
 
